1cd881f31741b18461c42aede2be20dd95581535
Ranon Rodrigues Cavalcante <ranonlp@gmail.com>
2018-09-01 23:03:42 -0400

    Configuração do Migration e o Entity Framework

---
+++ ProjetoModeloDDD/ProjetoModeloDDD.Infra.Data/Contexto/ProjetoModeloContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using ProjetoModeloDDD.Domain.Entities;
+
+namespace ProjetoModeloDDD.Infra.Data.Contexto
+{
+    public class ProjetoModeloContext : DbContext
+    {
+        public ProjetoModeloContext()
+            : base("ProjetoModeloDDD")
+        {
+
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
